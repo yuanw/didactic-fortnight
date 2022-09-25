e7c84a4291a99f4a9550a6f9fa4ad1d25e65e380
yuanw <me@yuanwang.ca>
2022-09-25 15:38:52 -0600

    try out effectful

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,53 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Main where
 
-import Main.Utf8 qualified as Utf8
+import Effectful (Eff, Effect, IOE, MonadIO (liftIO), runEff, type (:>))
+import Effectful.Dispatch.Dynamic (interpret)
+import Effectful.TH (makeEffect)
+
+-- import Main.Utf8 qualified as Utf8
 
 {- |
  Main entry point.
 
  The `bin/run` script will invoke this function.
 -}
+data Greeting :: Effect where
+  GetName :: Greeting m String
+  Greet :: String -> Greeting m ()
+
+makeEffect ''Greeting
+
+program :: (Greeting :> es) => Eff es ()
+program = do
+  name <- getName
+
+  greet name
+
+runGreeting :: (IOE :> es) => Eff (Greeting : es) a -> Eff es a
+runGreeting = interpret $ const \case
+  GetName -> liftIO getLine
+  Greet name -> liftIO $ putStrLn $ "Hello, " <> name <> "!"
+
+runProgram :: Eff '[Greeting, IOE] a -> IO a
+runProgram = runEff . runGreeting
+
 main :: IO ()
-main = do
-  -- For withUtf8, see https://serokell.io/blog/haskell-with-utf8
-  Utf8.withUtf8 $ do
-    putTextLn "Hello 🌎"
+main = runProgram program
+
+-- main :: IO ()
+-- main = do
+--   -- For withUtf8, see https://serokell.io/blog/haskell-with-utf8
+--   Utf8.withUtf8 $ do
+--     putTextLn "Hello 🌎"
---
+++ test/Spec.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeOperators #-}
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+
+module Spec (
+  spec,
+) where
+
+import Effectful (Eff, runPureEff)
+import Effectful.Dispatch.Dynamic (reinterpret)
+import Effectful.Writer.Dynamic qualified as Writer
+import Effects (Greeting (..), program)
+import Test.Hspec (Spec, describe, it, shouldBe)
+
+runGreeting :: String -> Eff (Greeting : es) a -> Eff es (a, String)
+runGreeting name = reinterpret Writer.runWriterLocal $ const \case
+  GetName -> pure name
+  Greet n -> Writer.tell $ "greet: " <> n
+
+runProgram :: String -> Eff '[Greeting] a -> (a, String)
+runProgram name = runPureEff . runGreeting name
+
+spec :: Spec
+spec = do
+  describe "program" do
+    it "greets us" do
+      let (result, greeting) = runProgram "Drew" program
+
+      result `shouldBe` ()
+      greeting `shouldBe` "greet: Drew"
