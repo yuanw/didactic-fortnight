37390a0d1c3431eafe521d11b64186754af51bab
yuanw <me@yuanwang.ca>
2022-09-29 14:30:16 -0600

    wip

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,16 +3,15 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TypeOperators #-}
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
 
-module Spec (
-  spec,
+module Main (
+  main,
 ) where
 
 import Effectful (Eff, runPureEff)
 import Effectful.Dispatch.Dynamic (reinterpret)
 import Effectful.Writer.Dynamic qualified as Writer
-import Test.Hspec (Spec, describe, it, shouldBe)
+import Test.Hspec (describe, hspec, it, shouldBe)
 import TryEffectful (Greeting (..), program)
 
 runGreeting :: String -> Eff (Greeting : es) a -> Eff es (a, String)
@@ -23,11 +22,11 @@ runGreeting name = reinterpret Writer.runWriterLocal $ const \case
 runProgram :: String -> Eff '[Greeting] a -> (a, String)
 runProgram name = runPureEff . runGreeting name
 
-spec :: Spec
-spec = do
+main :: IO ()
+main = hspec $ do
   describe "program" do
     it "greets us" do
       let (result, greeting) = runProgram "Drew" program
 
       result `shouldBe` ()
-      greeting `shouldBe` "greet: Drew1 "
+      greeting `shouldBe` "greet: Drew"
