30500a605eb38f5c1184921528b3cd3f738bd42e
yuanwang <yuan.wang@workiva.com>
2023-10-15 18:12:34 -0600

    n

--- little-schemer/lat?.scm
+++ little-schemer/lat?.scm
@@ -2,3 +2,10 @@
   (if (= n 0)
       1
       (* n (fact (- n 1)))))
+
+(define (lat? l)
+   (lambda (l)
+    (cond
+      ((null? l) #t)
+      ((atom? (car l)) (lat? (cdr l)))
+      (else #f))))
