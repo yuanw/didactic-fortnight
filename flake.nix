5aef93ab04294d02cbe5db77704417c9835f9a0f
yuanwang <yuan.wang@workiva.com>
2023-11-24 11:55:23 -0600

    wip

--- flake.nix
+++ flake.nix
@@ -15,6 +15,16 @@
     };
   };
 
+ nixConfig = {
+    extra-substituters = [
+      "https://cache.iog.io"
+    ];
+    extra-trusted-public-keys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+    allow-import-from-derivation = true;
+  };
+
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
